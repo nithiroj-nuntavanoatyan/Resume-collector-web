9b377b345ff15ae7a66c82a7994cc57d23cf40cf
nithiroj-nuntavanoatyan <nithiroj.n@kkumail.com>
2024-03-02 17:43:16 +0700

    make add resume function search by category

--- resume-collector/src/components/Searchbar.jsx
+++ resume-collector/src/components/Searchbar.jsx
@@ -1,18 +1,68 @@
 
+import { IoSearchSharp } from "react-icons/io5";
+import { useState } from "react";
+import { collection, orderBy, query, where } from "firebase/firestore";
+import { getDocs } from "firebase/firestore";
+import { db } from "../services/FirebaseConfig";
+import ResumeCard from "./ResumeCard";
 
 function Searchbar() {
 
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+    const [showNoResults, setShowNoResults] = useState(false);
+
+
+    const searchbar = async () => {
+        try {
+            // Create a query based on search criteria
+            const q = query(collection(db, "Resume"), where("category.Jobtitle", "==", searchTerm), orderBy("dateCreatedAt", "desc"));
+
+            // Execute the query and get results
+            const querySnapshot = await getDocs(q);
+
+            // Extract and return the matching documents
+            const results = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+            setSearchResults(results);
+            setShowNoResults(results.length === 0);
+        } catch (error) {
+            console.error("Error searching Firestore:", error);
+            return []; // Return an empty array on error
+        }
+    };
+
+    const searchbutton = (e) => {
+        e.preventDefault(); // Prevent default form submission
+        searchbar(); // Call the search function directly
+    };
+
     return (
-        <div className='search'>
+        <div>
+            <div className='search'>
             <div className="search-input flex justify-center items-center">
-                <div className="search-icon">
-                   
-                </div>
-                <input className="w-[230px] h-[6opx] sm:w-[450px] sm:h-[80px] text-lg sm:text-3xl p-4 text-center border-8 border-[#9793CD] rounded-lg font-semibold" type="text" placeholder="ใส่ชื่อ tag หรือชื่อที่ต้องการ"/>
+                <form action="searchbar " onSubmit={searchbutton}>
+                    <div className="searchbar flex justify-center items-center">
+                        <input className="w-[180px] h-[45px] sm:h-[60px] sm:w-[450px] text-base sm:text-3xl text-center border-4 border-[#9793CD] rounded-lg font-semibold" type="text" placeholder="ใส่ชื่อหมวดหมู่ที่ต้องการ" required value={searchTerm} onChange={(e) => { setSearchTerm(e.target.value) }} />
+                        <button className="searchbutton m-2 text-lg bg-[#2B5E53] p-2 rounded-md text-white"><span className="flex items-center" ><IoSearchSharp />ค้นหา</span></button>
+                    </div>
+                </form>
             </div>
-            <div className="dataresult">
+            
+        </div>
+        <div className="searchresult place-items-center grid md:grid-cols-2 lg:grid-cols-3">
+                {searchResults.length > 0 ? (
+                    searchResults.map((result) => (
+                        <ResumeCard key={result.name} resumeData={result} />
+                    ))
+                ) : showNoResults && (
+                    <div>
+                        <h1 className="noresult text-lg text-center">ขออภัย, ไม่พบสิ่งที่คุณค้นหา</h1>
+                    </div>
+                    
+                )}
             </div>
         </div>
+        
     )
 }
 
--- resume-collector/src/pages/Search.jsx
+++ resume-collector/src/pages/Search.jsx
@@ -1,4 +1,5 @@
-import Searchbar from "../components/searchbar"
+import Searchbar from "../components/Searchbar"
+
 
 
 function Search () {
@@ -7,9 +8,9 @@ function Search () {
     return (
         <section className='hero'>
             <div className="flex justify-center items-center">
-                <div className="text-2xl font-bold sm:text-5xl p-5 text-center"><h1>{thai_searchtitle}</h1></div>
+                <div className="text-2xl font-bold sm:text-5xl m-2 p-4 text-center"><h1>{thai_searchtitle}</h1></div>
             </div>
-            <div className="flex justify-center items-center">
+            <div className="">
                 <Searchbar/>
             </div>
             
